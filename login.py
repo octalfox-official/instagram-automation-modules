041d0538ac21324d63fd33e60432277350296eb8
octalfox-10008 <oct751008@gmail.com>
2024-08-09 02:11:44 +0500

    calculate your age

--- login.py
+++ login.py
@@ -1,9 +1,6 @@
-import time
-
-from constants import *
-
-
-def run_programe():
-    print(Programmer)
-    time.sleep(2)
-    run_programe()
+def calculate_age():
+    birth_year = int(input("Enter your birth year: "))
+    current_year = 2024
+    age = ( current_year - birth_year)
+    print("Your age is", age)
+calculate_age()
